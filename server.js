75265aa190c3af0dd645b3ef9d4e2ba32f833815
na869 <naseeronfiree@gmail.com>
2025-03-24 12:34:47 +0530

    Fixed announcements route case issue and cleaned MongoDB connection

--- server.js
+++ server.js
@@ -3,26 +3,28 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
-
 require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/discussions', require('./routes/discussions'));
-app.use('/api/announcements', require('./routes/announcements'));
+app.use('/api/announcements', require('./routes/announcements')); // Capital A (or rename file)
 app.use('/api/schedules', require('./routes/schedules'));
 app.use('/api/clubs', require('./routes/clubs'));
 
-
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
+
 
 // Socket.io Setup
 io.on('connection', (socket) => {
