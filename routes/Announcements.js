94941ffa756ade69949784faa5caa1f0e7da6617
na869 <naseeronfiree@gmail.com>
2025-03-24 14:11:31 +0530

    Add all route files and update server.js with io fix

--- routes/Announcements.js
+++ routes/Announcements.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const announcement = require('../models/announcement');
+const Announcement = require('../models/announcement');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
-router.get('/', async (req, res) => {
-  const announcements = await Announcement.find().populate('author', 'name');
-  res.json(announcements);
-});
+module.exports = (io) => {
+  router.get('/', async (req, res) => {
+    const announcements = await Announcement.find().populate('author', 'name');
+    res.json(announcements);
+  });
 
-router.post('/', [auth, admin], async (req, res) => {
-  const { title, content } = req.body;
-  const announcement = new Announcement({ title, content, author: req.user.id });
-  await announcement.save();
-  io.emit('newAnnouncement', announcement);
-  res.status(201).json(announcement);
-});
+  router.post('/', [auth, admin], async (req, res) => {
+    const { title, content } = req.body;
+    const announcement = new Announcement({ title, content, author: req.user.id });
+    await announcement.save();
+    io.emit('newAnnouncement', announcement);
+    res.status(201).json(announcement);
+  });
 
-module.exports = router;
+  return router;
+};
